3869bd6574a3197f6f52906e6ccf0dabfdcbcad1
slmwld02 <slmwld02@gmail.com>
2019-06-11 11:34:02 -0500

    commit del 11 de junio

---
+++ src/app/pages/popover/popover.page.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
+import { PopinfoComponent } from '../../components/popinfo/popinfo.component';
+
+@Component({
+  selector: 'app-popover',
+  templateUrl: './popover.page.html',
+  styleUrls: ['./popover.page.scss'],
+})
+export class PopoverPage implements OnInit {
+
+  constructor(private popoverCtrl: PopoverController) { }
+
+  ngOnInit() {
+  }
+async mostrarpop( evento ) {
+
+  const popover = await this.popoverCtrl.create({
+    component: PopinfoComponent,
+    event: evento,
+    mode: 'ios',
+    backdropDismiss: false
+  });
+  await popover.present();
+
+  const { data } = await popover.onDidDismiss();
+
+  console.log('Padre', data);
+
+}
+
+}
